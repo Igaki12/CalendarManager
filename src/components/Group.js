32d2e6070172667b952957954fbc89e203308c18
unknown <igatatsu1997@gmail.com>
2022-01-27 12:57:26 +0900

    first commit

---
+++ src/components/Group.js
@@ -0,0 +1,86 @@
+import {
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
+import {
+  SearchIcon,
+  TimeIcon,
+  CalendarIcon,
+  AtSignIcon,
+  InfoOutlineIcon,
+  ChevronDownIcon
+} from '@chakra-ui/icons';
+import { Flex } from '../../../todo-app/node_modules/@chakra-ui/layout/dist/chakra-ui-layout.cjs';
+export const Group = ({
+  id,
+  groupType,
+  groupTitle,
+  eventList,
+  additionalRowType,
+}) => {
+  const TitleIcon = (groupType) => {
+    if(groupType === 'search') return <SearchIcon />
+    else if(groupType === 'time') return <TimeIcon />
+    else if(groupType === 'calendar') return <CalendarIcon />
+    else if(groupType === 'at') return <AtSignIcon />
+    return <InfoOutlineIcon />
+  };
+  const GroupTitle = (id,groupType,groupTitle) => {
+    return(
+      <Flex>
+        {id}<TitleIcon groupType={groupType} />{groupTitle}
+      </Flex>
+    )
+  };
+  const EventList = (eventList) => {
+    return (
+      <MenuList minWidth='240px' type='checkbox' defaultChecked>
+        {eventList.title.map((value)=>{
+          <MenuItem>{value}</MenuItem>
+        })}
+      </MenuList>
+    )
+  };
+  const AdditionalRow = (additionalRowType) => {
+    if(additionalRowType === 'perHour') {
+      return(
+        <Flex>
+          時給 <Text type='number' />
+        </Flex>
+      )
+    }if(additionalRowType === 'perDay') {
+      return(
+        <Flex>
+          日給 <Text type='number' />
+        </Flex>
+      )
+    }else{
+      return;
+    }
+  };
+  let eventHours = (eventList) => {
+    eventList.time.reduce((sum,element)=> {
+      return sum + element;
+    },0);
+  }
+  return (
+    <>
+    <Menu>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}display="flex">
+        <GroupTitle id={id} groupType={groupType} groupTitle={groupTitle} />
+      </MenuButton>
+      <EventList eventList={eventList} />
+    </Menu>
+    <Flex>
+      個数  {eventList.length}
+    </Flex>
+    <Flex>
+      総時間  {eventHours(eventList)}
+    </Flex>
+    <AdditionalRow />
+    </>
+  )
+}
