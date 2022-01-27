56e58d1eaf64c6fe5d7c9cf74e67c21c6af32fed
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-01-28 00:33:28 +0900

    corrected syntax errors with CodeSandBox

--- src/components/Group.js
+++ src/components/Group.js
@@ -4,7 +4,9 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-} from '@chakra-ui/react';
+  Text,
+  Button
+} from "@chakra-ui/react";
 import {
   SearchIcon,
   TimeIcon,
@@ -12,75 +14,73 @@ import {
   AtSignIcon,
   InfoOutlineIcon,
   ChevronDownIcon
-} from '@chakra-ui/icons';
-import { Flex } from '@chakra-ui/react';
+} from "@chakra-ui/icons";
 export const Group = ({
   id,
   groupType,
   groupTitle,
   eventList,
-  additionalRowType,
+  additionalRowType
 }) => {
   const TitleIcon = (groupType) => {
-    if(groupType === 'search') return <SearchIcon />
-    else if(groupType === 'time') return <TimeIcon />
-    else if(groupType === 'calendar') return <CalendarIcon />
-    else if(groupType === 'at') return <AtSignIcon />
-    return <InfoOutlineIcon />
+    if (groupType === "search") return <SearchIcon />;
+    else if (groupType === "time") return <TimeIcon />;
+    else if (groupType === "calendar") return <CalendarIcon />;
+    else if (groupType === "at") return <AtSignIcon />;
+    return <InfoOutlineIcon />;
   };
-  const GroupTitle = (id,groupType,groupTitle) => {
-    return(
+  const GroupTitle = (id, groupType, groupTitle) => {
+    return (
       <Flex>
-        {id}<TitleIcon groupType={groupType} />{groupTitle}
+        {id}
+        <TitleIcon groupType={groupType} />
+        {groupTitle}
       </Flex>
-    )
+    );
   };
   const EventList = (eventList) => {
     return (
-      <MenuList minWidth='240px' type='checkbox' defaultChecked>
-        {eventList.title.map((value)=>{
+      <MenuList minWidth="240px" type="checkbox" defaultChecked>
+        {eventList.title.map((value) => (
           <MenuItem>{value}</MenuItem>
-        })}
+        ))}
       </MenuList>
-    )
+    );
   };
   const AdditionalRow = (additionalRowType) => {
-    if(additionalRowType === 'perHour') {
-      return(
+    if (additionalRowType === "perHour") {
+      return (
         <Flex>
-          時給 <Text type='number' />
+          時給 <Text type="number" />
         </Flex>
-      )
-    }if(additionalRowType === 'perDay') {
-      return(
+      );
+    }
+    if (additionalRowType === "perDay") {
+      return (
         <Flex>
-          日給 <Text type='number' />
+          日給 <Text type="number" />
         </Flex>
-      )
-    }else{
+      );
+    } else {
       return;
     }
   };
   let eventHours = (eventList) => {
-    eventList.time.reduce((sum,element)=> {
+    eventList.time.reduce((sum, element) => {
       return sum + element;
-    },0);
-  }
+    }, 0);
+  };
   return (
     <>
-    <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}display="flex">
-        <GroupTitle id={id} groupType={groupType} groupTitle={groupTitle} />
-      </MenuButton>
-      <EventList eventList={eventList} />
-    </Menu>
-    <Flex>
-      個数  {eventList.length}
-    </Flex>
-    <Flex>
-      総時間  {eventHours(eventList)}
-    </Flex>
-    <AdditionalRow />
+      <Menu>
+        <MenuButton as={Button} rightIcon={<ChevronDownIcon />} display="flex">
+          <GroupTitle id={id} groupType={groupType} groupTitle={groupTitle} />
+        </MenuButton>
+        <EventList eventList={eventList} />
+      </Menu>
+      <Flex>個数 {eventList.length}</Flex>
+      <Flex>総時間 {eventHours(eventList)}</Flex>
+      <AdditionalRow />
     </>
-  )
-}
+  );
+};
