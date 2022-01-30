0ff0ba344622804d187474c7752dc21df82cab43
unknown <igatatsu1997@gmail.com>
2022-01-30 19:22:36 +0900

    first commit

---
+++ src/apis/calendar.js
@@ -0,0 +1,33 @@
+import ApiCalendar from "react-google-calendar-api";
+import './App.css';
+function App(){
+  const getEvents = async() =>{
+    return new Promise(async (resolve,reject) => {
+      if(ApiCalendar.sign){
+        ApiCalendar.listEvents({
+          timeMin : new Date().toISOString(),
+          timeMax : new Date().addDays(10).toISOString(),
+          showDeleted: true,
+          maxResults: 10,
+          orderBy:'updated'
+        }).then(({result}) => {
+          if(result.items){
+            console.log("Events From Calendar",result.items);
+          }else{
+            console.log("No Events")
+          }
+          resolve(result);
+        });
+      }else{
+        ApiCalendar.handleAuthClick();
+        resolve(null);
+      }
+    })
+  };
+  return(
+    <div className="App">
+      <button onClick="()=> getEvents()">Get Events</button>
+    </div>
+  );
+}
+export default App;
