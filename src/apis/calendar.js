edc0fa39aec2034082c76341f8ed71345a3be4a5
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-01-30 19:59:19 +0900

    added what to get from GCP-API

--- src/apis/calendar.js
+++ src/apis/calendar.js
@@ -1,30 +1,31 @@
 import ApiCalendar from "react-google-calendar-api";
-import './App.css';
-function App(){
-  const getEvents = async() =>{
-    return new Promise(async (resolve,reject) => {
-      if(ApiCalendar.sign){
+import "./App.css";
+function App() {
+  const getEvents = async () => {
+    return new Promise(async (resolve, reject) => {
+      if (ApiCalendar.sign) {
         ApiCalendar.listEvents({
-          timeMin : new Date().toISOString(),
-          timeMax : new Date().addDays(10).toISOString(),
+          timeMin: new Date().toISOString(),
+          timeMax: new Date().addDays(10).toISOString(),
           showDeleted: true,
           maxResults: 10,
-          orderBy:'updated'
-        }).then(({result}) => {
-          if(result.items){
-            console.log("Events From Calendar",result.items);
-          }else{
-            console.log("No Events")
+          orderBy: "updated"
+        }).then(({ result }) => {
+          if (result.items) {
+            console.log("Events From Calendar", result.items);
+          } else {
+            console.log("No Events");
           }
           resolve(result);
         });
-      }else{
+      } else {
         ApiCalendar.handleAuthClick();
         resolve(null);
       }
-    })
+      console.log(getEvents);
+    });
   };
-  return(
+  return (
     <div className="App">
       <button onClick="()=> getEvents()">Get Events</button>
     </div>
